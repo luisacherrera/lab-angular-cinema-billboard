e84f3681921dc97cdee97e574aabad96e08e36cc
Luis Herrera <la.herrera457@gmail.com>
2018-02-21 23:52:08 +0100

    stuck on iteration 4

--- starter-code/src/app/components/my-home/my-home.component.ts
+++ starter-code/src/app/components/my-home/my-home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MoviesService } from '../../services/movies.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-my-home',
@@ -6,10 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./my-home.component.css']
 })
 export class MyHomeComponent implements OnInit {
+  movies: Array<Object>;
 
-  constructor() { }
+  constructor(private moviesService : MoviesService, private router: Router) { 
+    this.movies = this.moviesService.getMovies();
+  }
 
   ngOnInit() {
+  }
+
+  goToMovie(id) {
+    this.router.navigate(['/movie', id])
   }
 
 }
--- starter-code/src/app/components/my-movie/my-movie.component.ts
+++ starter-code/src/app/components/my-movie/my-movie.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MoviesService } from '../../services/movies.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-my-movie',
@@ -6,10 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./my-movie.component.css']
 })
 export class MyMovieComponent implements OnInit {
+  id: Number;
+  movie: any;
 
-  constructor() { }
+  constructor(private moviesService : MoviesService, private activatedRoute: ActivatedRoute) { 
+    this.activatedRoute.params
+    .subscribe(params => this.id =params.id);
+  }
 
   ngOnInit() {
+    this.movie = this.moviesService.getMovie(this.id);
+    console.log(this.movie);
   }
 
 }
